f1468c38c08c0bde40a5c8dfc5535338ef19aa4b
chalaa <chalaolanigeleta@gmail.com>
2025-06-08 18:54:18 +0300

    remove custome role and permission to add spatie

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ use App\Http\Middleware\JwtMiddleware;
 Route::post('/register', [AuthController::class,'register'])->name('register_user');
 Route::post('/login', [AuthController::class, 'login'])->name('login_user');
 Route::post('/refresh',[AuthController::class, 'refresh'])->name('refresh_token');
-Route::middleware([JwtMiddleware::class])->group(function () {
+Route::middleware(['auth:api'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout_user');
 });
